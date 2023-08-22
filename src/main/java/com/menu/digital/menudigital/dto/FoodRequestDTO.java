4d16b840d0f92e21b33e4be44b69c1d3d3333df9
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-22 12:18:39 -0300

    Adicionando entidade food e foodDTO para retorno dos dados

---
+++ src/main/java/com/menu/digital/menudigital/dto/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.menu.digital.menudigital.dto;
+
+public record FoodRequestDTO(String title, String image, Integer price){
+
+}
---
+++ src/main/java/com/menu/digital/menudigital/dto/FoodResponseDTO.java
@@ -0,0 +1,9 @@
+package com.menu.digital.menudigital.dto;
+
+import com.menu.digital.menudigital.entity.Food;
+
+public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+    public FoodResponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
