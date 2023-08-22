460a7bdd39555fc7c39151041658c725b08d355c
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-22 12:57:48 -0300

    Alterando crudRepository para jpa repostory

--- src/main/java/com/menu/digital/menudigital/repository/FoodRepository.java
+++ src/main/java/com/menu/digital/menudigital/repository/FoodRepository.java
@@ -1,7 +1,7 @@
 package com.menu.digital.menudigital.repository;
 
 import com.menu.digital.menudigital.entity.Food;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FoodRepository extends CrudRepository<Long, Food> {
+public interface FoodRepository extends JpaRepository<Food, Long> {
 }
