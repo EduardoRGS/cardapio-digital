c106115b30b58a8b92bd763669a376d9ab2b4269
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-22 12:21:13 -0300

    Adicionando food repository

---
+++ src/main/java/com/menu/digital/menudigital/repository/FoodRepository.java
@@ -0,0 +1,7 @@
+package com.menu.digital.menudigital.repository;
+
+import com.menu.digital.menudigital.entity.Food;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FoodRepository extends CrudRepository<Long, Food> {
+}
