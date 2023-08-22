a592f8ce25f52b5ff141306a07aa7d5987ffdbf0
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-22 11:09:06 -0300

    initial commit

---
+++ src/test/java/com/menu/digital/menudigital/MenuDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menu.digital.menudigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
