6247ad29c9be08aaccb590f8e5ec6d11d548f8e1
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-08-24 22:44:39 -0300

    Adicionando cron para comunicar os endpoints com o frontend

--- src/main/java/com/menu/digital/controller/FoodController.java
+++ src/main/java/com/menu/digital/controller/FoodController.java
@@ -4,11 +4,7 @@ import com.menu.digital.dto.FoodRequestDTO;
 import com.menu.digital.dto.FoodResponseDTO;
 import com.menu.digital.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +15,13 @@ public class FoodController {
     @Autowired
     private FoodService foodService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping( "/addFood")
     public void create(@RequestBody FoodRequestDTO data) {
         foodService.createItemOnMenu(data);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/listFoods")
     List<FoodResponseDTO> listFoods() {
         return foodService.listAllMenu();
